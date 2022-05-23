6ed9628a4ed5ba952f7d9e3edeeaa0b0f4b73120
NatalaBoiko <natalabojko@gmail.com>
2022-05-23 22:52:20 +0300

    have begun

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(onUserRequest, DEBOUNCE_DELAY));
+
+function onUserRequest(e) {
+  const userRequest = e.target.value.trim();
+
+  fetchCountries(userRequest)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+//=======================================
+
+// fetch('https://restcountries.com/v2/name/CHINA')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(name => {
+//     console.log(name);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,18 @@
+export default function fetchCountries(name) {
+  return fetch(
+    'https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages',
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Error fetchin data');
+  });
+}
+// export default function fetchCountries(name) {
+//   return fetch('https://restcountries.com/v2/name/china').then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error('Error fetchin data');
+//   });
+// }
